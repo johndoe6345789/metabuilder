f088476e0ed0a440942f879ef55171a9e359952f
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-25 20:49:08 +0000

    code: nextjs,index,frontends (2 files)

--- frontends/nextjs/src/lib/packages/index.ts
+++ frontends/nextjs/src/lib/packages/index.ts
@@ -1,6 +1,6 @@
 // Package system exports
 export * from './package-types'
-export { initializePackageSystem, buildPackageRegistry } from './package-loader'
-export { exportAllPackagesForSeed } from './package-export'
-export { packageCatalog } from './package-catalog'
+export { initializePackageSystem, getPackageRegistry } from './package-loader'
+export { exportPackagesForSeed } from './package-export'
+export { PACKAGE_CATALOG } from './package-catalog'
 export { packageGlue, getPackageGlue } from './package-glue'
