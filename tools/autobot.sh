5f49f77e65ea6e5cc482c0beb7352d31ead478a1
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-25 20:45:13 +0000

    scripts: tools,autobot (1 files)

--- tools/autobot.sh
+++ tools/autobot.sh
@@ -37,5 +37,5 @@ while :; do
   sleep 90
 
   # IMPORTANT: use normal ASCII quotes, not “smart quotes”.
-  codex exec --full-auto --sandbox="danger-full-access" --cd="/Users/rmac/Documents/GitHub/metabuilder/docs/todo/" "Please resolve my TODO list" || exit $?
+  codex exec --full-auto --sandbox="danger-full-access" --cd="/Users/rmac/Documents/GitHub/metabuilder/docs/todo/" "Please resolve my TODO list, Scan project for TODO items." || exit $?
 done
