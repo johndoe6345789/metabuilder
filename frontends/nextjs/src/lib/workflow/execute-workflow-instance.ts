04a449132cf9af770d93dda1aaaa3c4105791151
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-25 22:56:58 +0000

    code: workflow,nextjs,instance (1 files)

--- frontends/nextjs/src/lib/workflow/execute-workflow-instance.ts
+++ frontends/nextjs/src/lib/workflow/execute-workflow-instance.ts
@@ -1,14 +1,14 @@
 import type { Workflow } from '../types/level-types'
 import type { WorkflowExecutionContext } from './workflow-execution-context'
 import type { WorkflowExecutionResult } from './workflow-execution-result'
-import type { WorkflowEngine } from './workflow-engine-class'
 import { executeWorkflow } from './execute-workflow'
 
 /**
  * Convenience instance method for legacy workflow execution
+ * Note: this function doesn't use 'this' - it's a passthrough to executeWorkflow
  */
 export async function executeWorkflowInstance(
-  this: WorkflowEngine,
+  this: unknown,
   workflow: Workflow,
   context: WorkflowExecutionContext
 ): Promise<WorkflowExecutionResult> {
