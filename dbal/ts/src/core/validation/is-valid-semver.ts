ac1198b1aa3a57d73b2a2babc4153b299d325752
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-25 19:33:38 +0000

    code: valid,semver,is (1 files)

--- dbal/ts/src/core/validation/is-valid-semver.ts
+++ dbal/ts/src/core/validation/is-valid-semver.ts
@@ -1,4 +1,4 @@
-const semverPattern = /^\\d+\\.\\d+\\.\\d+$/
+const semverPattern = /^\d+\.\d+\.\d+$/
 
 export function isValidSemver(version: string): boolean {
   return semverPattern.test(version)
