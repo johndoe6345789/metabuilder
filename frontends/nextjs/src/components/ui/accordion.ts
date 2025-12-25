538ed9acc62dabe401869e409255d5b9d4efc12c
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-25 19:46:54 +0000

    code: valid,nextjs,is (4 files)

---
+++ frontends/nextjs/src/components/ui/accordion.ts
@@ -0,0 +1,3 @@
+// Re-export for backward compatibility
+// TODO: Update imports to use @/components/ui directly
+export { Accordion, AccordionItem, AccordionTrigger, AccordionContent } from './molecules/Accordion'
--- frontends/nextjs/src/lib/utils.ts
+++ frontends/nextjs/src/lib/utils.ts
@@ -1,6 +1,9 @@
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
 
+/**
+ * Utility function to merge class names
+ * Note: Project uses MUI, not Tailwind. This function just concatenates class names.
+ */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return clsx(inputs)
 }
