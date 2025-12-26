19559024e6301e9bde10b3ae2cc70c670ce50911
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 03:37:01 +0000

    code: nextjs,frontends,set (3 files)

--- frontends/nextjs/src/lib/db/credentials/crud/operations/get-credentials.ts
+++ frontends/nextjs/src/lib/db/credentials/crud/operations/get-credentials.ts
@@ -1,4 +1,4 @@
-import { getAdapter } from '../dbal-client'
+import { getAdapter } from '../../../core/dbal-client'
 
 /**
  * Get all credentials as a username->passwordHash map
--- frontends/nextjs/src/lib/db/credentials/crud/operations/set-credential.ts
+++ frontends/nextjs/src/lib/db/credentials/crud/operations/set-credential.ts
@@ -1,4 +1,4 @@
-import { getAdapter } from '../dbal-client'
+import { getAdapter } from '../../../core/dbal-client'
 
 /**
  * Set or update a user's credential
