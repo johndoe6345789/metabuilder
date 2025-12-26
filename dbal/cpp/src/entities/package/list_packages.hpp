6a716aa003b3e36c37ed78be7904fd248e53a9ac
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 01:27:23 +0000

    code: packages,list,hpp (1 files)

--- dbal/cpp/src/entities/package/list_packages.hpp
+++ dbal/cpp/src/entities/package/list_packages.hpp
@@ -19,18 +19,32 @@ namespace package {
  */
 inline Result<std::vector<Package>> list(InMemoryStore& store, const ListOptions& options) {
     std::vector<Package> packages;
-    
-    for (const auto& [id, pkg] : store.packages) {
+
+    for (const auto& [id, package] : store.packages) {
         bool matches = true;
-        
-        if (options.filter.find("is_active") != options.filter.end()) {
-            bool filter_active = options.filter.at("is_active") == "true";
-            if (pkg.is_active != filter_active) matches = false;
+
+        if (options.filter.find("name") != options.filter.end()) {
+            if (package.name != options.filter.at("name")) matches = false;
         }
-        
-        if (matches) packages.push_back(pkg);
+
+        if (options.filter.find("version") != options.filter.end()) {
+            if (package.version != options.filter.at("version")) matches = false;
+        }
+
+        if (options.filter.find("author") != options.filter.end()) {
+            if (package.author != options.filter.at("author")) matches = false;
+        }
+
+        if (options.filter.find("is_installed") != options.filter.end()) {
+            bool filter_installed = options.filter.at("is_installed") == "true";
+            if (package.is_installed != filter_installed) matches = false;
+        }
+
+        if (matches) {
+            packages.push_back(package);
+        }
     }
-    
+
     if (options.sort.find("name") != options.sort.end()) {
         std::sort(packages.begin(), packages.end(), [](const Package& a, const Package& b) {
             return a.name < b.name;
@@ -40,14 +54,14 @@ inline Result<std::vector<Package>> list(InMemoryStore& store, const ListOptions
             return a.created_at < b.created_at;
         });
     }
-    
+
     int start = (options.page - 1) * options.limit;
     int end = std::min(start + options.limit, static_cast<int>(packages.size()));
-    
+
     if (start < static_cast<int>(packages.size())) {
         return Result<std::vector<Package>>(std::vector<Package>(packages.begin() + start, packages.begin() + end));
     }
-    
+
     return Result<std::vector<Package>>(std::vector<Package>());
 }
 
