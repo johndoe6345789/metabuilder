6cc9dbf1cf653b0666b706a056d1a1ab6c260d98
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 04:15:40 +0000

    code: workflow,nextjs,frontends (1 files)

--- frontends/nextjs/src/lib/workflow/engine/workflow-engine.ts
+++ frontends/nextjs/src/lib/workflow/engine/workflow-engine.ts
@@ -6,14 +6,19 @@
 export {
   executeWorkflow,
   executeNode,
-  createWorkflowState,
-  logToWorkflow,
-  WorkflowEngine,
-  createWorkflowEngine,
-} from './index'
+} from '../execution/execute-workflow'
 
-export type {
-  WorkflowExecutionContext,
-  WorkflowExecutionResult,
-  WorkflowState,
-} from './index'
+export { executeNode } from '../execution/execute-node'
+export { executeActionNode } from '../nodes/execute-action-node'
+export { executeConditionNode } from '../nodes/execute-condition-node'
+export { executeLuaNode } from '../nodes/execute-lua-node'
+export { executeTransformNode } from '../nodes/execute-transform-node'
+export { executeLuaCode } from '../nodes/execute-lua-code'
+export { createWorkflowState } from '../workflow-state'
+export { logToWorkflow } from '../log-to-workflow'
+export { WorkflowEngine } from './workflow-engine-class'
+export { createWorkflowEngine } from './create-workflow-engine'
+
+export type { WorkflowExecutionContext } from '../workflow-execution-context'
+export type { WorkflowExecutionResult } from '../workflow-execution-result'
+export type { WorkflowState } from '../workflow-state'
