265a09f9b43ae3379e35b9ffced8257f902433d4
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 03:26:42 +0000

    code: nextjs,frontends,useresolveduser (2 files)

--- frontends/nextjs/src/hooks/data/useResolvedUser.ts
+++ frontends/nextjs/src/hooks/data/useResolvedUser.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { Database } from '@/lib/database'
 import type { User } from '@/lib/level-types'
-import { useAuth } from '@/hooks/useAuth'
+import { useAuth } from '../useAuth'
 
 export interface ResolvedUserState {
   user: User | null
