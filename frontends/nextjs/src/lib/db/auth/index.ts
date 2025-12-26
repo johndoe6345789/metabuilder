0cbabf3a6cc69b47b7b1c1b39f60459222c570a9
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 03:26:11 +0000

    code: nextjs,index,frontends (2 files)

--- frontends/nextjs/src/lib/db/auth/index.ts
+++ frontends/nextjs/src/lib/db/auth/index.ts
@@ -1,4 +1,4 @@
-export { authenticateUser } from './authenticate-user'
-export type { AuthenticateResult } from './authenticate-user'
-export { getUserByUsername } from './get-user-by-username'
-export { getUserByEmail } from './get-user-by-email'
+export { authenticateUser } from './queries/authenticate-user'
+export type { AuthenticateResult } from './queries/authenticate-user'
+export { getUserByUsername } from './queries/get-user-by-username'
+export { getUserByEmail } from './queries/get-user-by-email'
