9fd191fb4f356acf870ccc8a62ea885bd4ee7b15
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 05:47:15 +0000

    config: frontends,dbal,nextjs (7 files)

--- frontends/nextjs/src/lib/db/users/super-god/transfer-super-god-power.ts
+++ frontends/nextjs/src/lib/db/users/super-god/transfer-super-god-power.ts
@@ -1,9 +1,8 @@
-import { dbalUpdateUser } from '@/lib/dbal/database-dbal/users/dbal-update-user.server'
-
 /**
  * Transfer SuperGod power from one user to another
  */
 export async function transferSuperGodPower(fromUserId: string, toUserId: string): Promise<void> {
+  const { dbalUpdateUser } = await import('@/lib/dbal/database-dbal/users/dbal-update-user.server')
   await dbalUpdateUser(fromUserId, { isInstanceOwner: false, role: 'god' })
   await dbalUpdateUser(toUserId, { isInstanceOwner: true, role: 'supergod' })
 }
