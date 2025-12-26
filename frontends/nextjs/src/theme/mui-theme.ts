89e1216a47a6201cd9bebfbc1593ee9670cf138e
JohnDoe6345789 <john.doe6345789@gmail.com>
2025-12-26 04:21:08 +0000

    code: theme,nextjs,mui (1 files)

--- frontends/nextjs/src/theme/mui-theme.ts
+++ frontends/nextjs/src/theme/mui-theme.ts
@@ -1,8 +1,8 @@
 'use client'
 
 // Re-export from modular theme files
-export { lightTheme } from './light-theme'
-export { darkTheme } from './dark-theme'
+export { lightTheme } from './modes/light-theme'
+export { darkTheme } from './modes/dark-theme'
 export { colors } from './colors'
 export { fonts } from './fonts'
 export { layout } from './layout'
