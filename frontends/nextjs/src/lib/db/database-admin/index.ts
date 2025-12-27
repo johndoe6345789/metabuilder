086db10f74a6ae697ca30e11077227c931a1a7d0
johndoe6345789 <john.doe6345789@gmail.com>
2025-12-27 18:00:57 +0000

    refactor: modularize default data seeding

--- frontends/nextjs/src/lib/db/database-admin/index.ts
+++ frontends/nextjs/src/lib/db/database-admin/index.ts
@@ -1,4 +1,4 @@
 export { clearDatabase } from './clear-database'
 export { exportDatabase } from './export-database'
 export { importDatabase } from './import-database'
-export { seedDefaultData } from './seed-default-data'
+export { seedDefaultData, defaultDataBuilders } from './seed-default-data'
