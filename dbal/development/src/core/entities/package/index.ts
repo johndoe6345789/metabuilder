b55bc9aeccdbd1166be7b90c9d9d9c7d9ede64f9
Richard Ward <JohnDoe6345789@gmail.com>
2025-12-30 20:26:06 +0000

    code: index,development,dbal (1 files)

--- dbal/development/src/core/entities/package/index.ts
+++ dbal/development/src/core/entities/package/index.ts
@@ -1,9 +1,32 @@
 /**
  * @file index.ts
  * @description Barrel export for package operations
+ * NOTE: Package operation files not yet implemented - stubbed for build
  */
-export { createPackage } from './create-package';
-export { getPackage, getPackageByPackageId } from './get-package';
-export { updatePackage } from './update-package';
-export { deletePackage } from './delete-package';
-export { listPackages } from './list-packages';
+
+// TODO: Implement these package operation files
+// export { createPackage } from './create-package';
+// export { getPackage, getPackageByPackageId } from './get-package';
+// export { updatePackage } from './update-package';
+// export { deletePackage } from './delete-package';
+// export { listPackages } from './list-packages';
+
+// Temporary stubs to allow build to proceed
+export const createPackage = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
+export const getPackage = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
+export const getPackageByPackageId = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
+export const updatePackage = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
+export const deletePackage = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
+export const listPackages = async (...args: any[]): Promise<any> => {
+  throw new Error('Package operations not yet implemented');
+};
