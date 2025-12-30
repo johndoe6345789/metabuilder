92f5aaf22b166c36a01922033220db8a60f14c9e
Richard Ward <JohnDoe6345789@gmail.com>
2025-12-30 20:40:00 +0000

    code: types,development,dbal (4 files)

---
+++ dbal/development/src/core/validation/entities/types.ts
@@ -0,0 +1,18 @@
+/**
+ * @file types.ts
+ * @description Entity validation types (stub)
+ */
+
+export interface ValidationResult {
+  isValid: boolean;
+  errors: string[];
+}
+
+export type User = any;
+export type Credential = any;
+export type Session = any;
+export type Page = any;
+export type ComponentHierarchy = any;
+export type Workflow = any;
+export type LuaScript = any;
+export type Package = any;
