ae8f7f911f22a3bbce14ee07077ede8b03416b26
Richard Ward <JohnDoe6345789@gmail.com>
2025-12-30 20:07:30 +0000

    code: server,nextjs,frontends (5 files)

--- frontends/nextjs/src/lib/dbal/database-dbal/core/initialize-dbal.server.ts
+++ frontends/nextjs/src/lib/dbal/database-dbal/core/initialize-dbal.server.ts
@@ -1,4 +1,4 @@
-import 'server-only'
+// import 'server-only'
 
 import { DBALClient } from '@/dbal'
 import type { DBALConfig } from '@/dbal/runtime/config'
--- frontends/nextjs/src/lib/dbal/database-dbal/users/dbal-delete-user.server.ts
+++ frontends/nextjs/src/lib/dbal/database-dbal/users/dbal-delete-user.server.ts
@@ -1,4 +1,4 @@
-import 'server-only'
+// import 'server-only'
 
 import { getDBAL } from '../core/get-dbal.server'
 
