076005ede3df3a161b875b8a446152ace80f0d6d
Richard Ward <JohnDoe6345789@gmail.com>
2025-12-30 14:10:52 +0000

    config: tsx,icons,fakemui (11 files)

---
+++ fakemui/icons/StarBorder.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { Icon, IconProps } from './Icon'
+
+export const StarBorder = (props: IconProps) => (
+  <Icon {...props}>
+    <path d="M128 32 L152 104 L224 104 L168 144 L192 216 L128 176 L64 216 L88 144 L32 104 L104 104 Z" />
+  </Icon>
+)
