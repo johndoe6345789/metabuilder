51e986861a317cfc3d0a1e47885bc2dabc088b9e
johndoe6345789 <john.doe6345789@gmail.com>
2026-08-07 22:17:57 +0100

    Extract namespace rehome logic to service
    NamespacesCtrl now includes `NamespaceRehome.hpp` and drops its local helper implementations for finding a default namespace and rehoming snippets. This centralizes namespace-deletion rehome behavior in a dedicated service, reducing controller duplication and keeping ownership/removal flow cleaner.

--- frontends/pastebin/backend2/src/controllers/NamespacesCtrl_remove.cpp
+++ frontends/pastebin/backend2/src/controllers/NamespacesCtrl_remove.cpp
@@ -2,44 +2,10 @@
 #include "AuthHelpers.hpp"
 #include "../services/DbalClient.hpp"
 #include "../services/NamespaceOwnership.hpp"
+#include "../services/NamespaceRehome.hpp"
 #include "../util.hpp"
 
 namespace pastebin {
-
-namespace {
-
-std::string findDefaultNamespaceId(const std::string& userId,
-                                    const std::string& excludeId) {
-    const auto all = dbalAllPages("/" + dbalTenant() +
-                                   "/pastebin/Namespace?filter.userId=" +
-                                   userId);
-    for (const auto& ns : all) {
-        if (ns.value("isDefault", false) &&
-            ns.value("id", std::string()) != excludeId)
-            return ns.value("id", "");
-    }
-    return "";
-}
-
-bool rehomeSnippets(const std::string& namespaceId, const std::string& userId,
-                     const std::string& defaultNsId) {
-    const auto snippets =
-        dbalAllPages("/" + dbalTenant() +
-                     "/pastebin/Snippet?filter.namespaceId=" + namespaceId);
-    for (auto snippet : snippets) {
-        if (snippet.value("userId", "") != userId)
-            continue;
-        snippet["namespaceId"] = defaultNsId;
-        const auto sid = snippet.value("id", "");
-        const auto r = dbalRequest(
-            "PUT", "/" + dbalTenant() + "/pastebin/Snippet/" + sid, &snippet);
-        if (!r || !r->ok())
-            return false;
-    }
-    return true;
-}
-
-} // namespace
 
 void NamespacesCtrl::remove(const drogon::HttpRequestPtr& req,
                              ResponseCb&& cb, const std::string& id) {
